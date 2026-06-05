c5ff59681356c294c07cca18d765383f74a10c02
ashwini tete <ashwinitete0@gmail.com>
2026-06-05 21:27:18 +0530

    new fun addition

--- FIRST.C
+++ FIRST.C
@@ -23,10 +23,54 @@ return 0;
 
 
 }*/
+/*
+
 #include <stdio.h>
 int main(void)
 {
     char ch = 'A';
     printf("The value of character is %c\n", ch);
     return 0;   
+}
+*/
+
+/*
+// this is three number swep program.
+#include <stdio.h>
+int main(void)
+{
+    int a ,b,c,next;
+    a=10;
+    b=20;   
+    c=30;
+    printf("The value of a is %d\n",a);
+    printf("The value of b is %d\n",b); 
+    printf("The value of c is %d\n",c);
+    next=a;
+    a=b;
+    b=c;
+    c=next;
+    printf("The swept value of a is %d\n",a);
+    printf("The swept value of b is %d\n",b);     
+    printf("The swept value of c is %d\n",c);
+return 0;
+}*/
+
+#include <stdio.h>
+int get(int a,int b);
+int main(void)  
+{
+   int a,b;
+   printf("Enter the value of a and b: ");  
+   scanf("%d %d",&a,&b);
+    get(a,b);
+    
+return 0;
+    
+}
+get (int a,int b)
+{
+    int sum;
+    sum=a+b;
+    printf("The sum of a and b is %d\n",sum);
 }
